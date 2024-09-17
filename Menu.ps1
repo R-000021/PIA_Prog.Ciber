5b5874da3f282becf26442dfc2e596b88a742d5b
R-000021 <abrammzeo17@gmail.com>
2024-09-16 22:18:07 -0600

    Mas cambios

--- Menu.ps1
+++ Menu.ps1
@@ -1,40 +1,52 @@
-﻿#Script Principal del menu
+﻿
+<#
+.SYNOPSIS
+Programa que contiene el menu en el que elijes realizar alguna de las funciones de los modulos
 
-function Menu{
+.DESCRIPTION
+La función Menu Desplega el menu para eligir alguna de la opciones
 
-$OP=Read-Host @"
+.EXAMPLE
 Elije una de la opciones del menu: 
     1.Consulta del API 
     2.Archivos Ocultos 
     3.Uso de recursos 
     4.Analisis del Firewall
 
-"@ 
-try{
+#>
 
-    switch ($OP){
-    1{
-        Import-Module C:\Users\AbrahamEstudillo\PIA\VirusTotal.psm1
-        VirusTotal_Report
-    }
-    2{
-        Import-Module C:\Users\AbrahamEstudillo\PIA\ArchOc.psm1
-        Hidden_Files
-    }
-    3{
-        Import-Module C:\Users\AbrahamEstudillo\PIA\UsoSistema.psm1
-        System_Results  
-    }
-    4{        
-        Import-Module C:\Users\AbrahamEstudillo\PIA\Firewall.psm1
-        Check_Firewall
-    }
+#Script Principal del menu
+function Menu{
+    $OP=Read-Host @"
+    Elije una de la opciones del menu: 
+        1.Consulta del API 
+        2.Archivos Ocultos 
+        3.Uso de recursos 
+        4.Analisis del Firewall
+"@ 
+    try{
+        switch ($OP){
+        1{
+            Import-Module C:\Users\AbrahamEstudillo\PIA\VirusTotal.psm1
+            VirusTotal_Report
+        }
+        2{
+            Import-Module C:\Users\AbrahamEstudillo\PIA\ArchOc.psm1
+            Hidden_Files
+        }
+        3{
+            Import-Module C:\Users\AbrahamEstudillo\PIA\UsoSistema.psm1
+            System_Results  
+        }
+        4{        
+            Import-Module C:\Users\AbrahamEstudillo\PIA\Firewall.psm1
+            Check_Firewall
+        }
 }
     
-}catch{
-    Write-Host "No se encontro ningun modulo con ese nombre" 
+    }catch{
+        Write-Host "No se encontro ningun modulo con ese nombre" 
+    }
 }
-}
-
 
 Menu
